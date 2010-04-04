bf7c3abb5e648c6e92d5d3ad44a50319e512292e
Greg Combs <gcombs@gmail.com>
2010-04-04 05:24:40 +0000

    Deleting build directory ... moved release binaries out first

--- build/TexLege.build/Release-iphoneos/TexLege.build/DerivedSources/TexLege_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char TexLegeVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:TexLege  PROJECT:TexLege-1.0" "\n"; const double TexLegeVersionNumber __attribute__ ((used)) = (double)1.0;
