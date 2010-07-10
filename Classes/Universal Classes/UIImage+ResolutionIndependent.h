2a36bb24ec09a75f8c07c32a759ff1bb44be6e3e
Greg Combs <gcombs@gmail.com>
2010-07-10 05:57:24 +0000

    Added utility classes

---
+++ Classes/Universal Classes/UIImage+ResolutionIndependent.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+ResolutionIndependent.h
+//  TexLege
+//
+//  Created by Gregory Combs on 7/9/10.
+//	http://atastypixel.com/blog/uiimage-resolution-independence-and-the-iphone-4s-retina-display/
+//
+
+
+@interface UIImage (ResolutionIndependent)
+- (id)initWithContentsOfResolutionIndependentFile:(NSString *)path;
++ (UIImage*)imageWithContentsOfResolutionIndependentFile:(NSString *)path;
+@end
---
+++ Classes/Universal Classes/UIImage+ResolutionIndependent.m
@@ -0,0 +1,34 @@
+//
+//  UIImage+ResolutionIndependent.m
+//  TexLege
+//
+//  Created by Gregory Combs on 7/9/10.
+//	http://atastypixel.com/blog/uiimage-resolution-independence-and-the-iphone-4s-retina-display/
+//
+
+
+#import "UIImage+ResolutionIndependent.h"
+
+
+@implementation UIImage (ResolutionIndependent)
+
+- (id)initWithContentsOfResolutionIndependentFile:(NSString *)path {
+    if ( [UIScreen instancesRespondToSelector:@selector(scale)] && [[UIScreen mainScreen] scale] == 2.0 ) {
+        NSString *path2x = [[path stringByDeletingLastPathComponent] 
+                            stringByAppendingPathComponent:[NSString stringWithFormat:@"%@@2x.%@", 
+                                                            [[path lastPathComponent] stringByDeletingPathExtension], 
+                                                            [path pathExtension]]];
+		
+        if ( [[NSFileManager defaultManager] fileExistsAtPath:path2x] ) {
+            return [self initWithContentsOfFile:path2x];
+        }
+    }
+	
+    return [self initWithContentsOfFile:path];
+}
+
++ (UIImage*)imageWithContentsOfResolutionIndependentFile:(NSString *)path {
+    return [[[UIImage alloc] initWithContentsOfResolutionIndependentFile:path] autorelease];
+}
+
+@end
