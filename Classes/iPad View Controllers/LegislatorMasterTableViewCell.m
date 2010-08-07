c36a736ea2d6db56337aebfaeae2e386ae3f370a
Greg Combs <gcombs@gmail.com>
2010-08-07 04:42:03 +0000

    Memory allocation. Frequent images in cells get loaded asynchronously

--- Classes/iPad View Controllers/LegislatorMasterTableViewCell.m
+++ Classes/iPad View Controllers/LegislatorMasterTableViewCell.m
@@ -12,7 +12,6 @@
 #import "LegislatorObj.h"
 #import "UtilityMethods.h"
 #import "TexLegeTheme.h"
-#import "UIImage+ResolutionIndependent.h"
 #import "ImageCache.h"
 @interface LegislatorMasterTableViewCell (Private)
 
