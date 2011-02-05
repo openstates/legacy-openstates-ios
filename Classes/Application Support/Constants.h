29660c74cb2c032401328ed33d464268d066c355
Greg Combs <gcombs@gmail.com>
2011-02-05 08:57:14 +0000

    Uses new shared library for common utility stuff

--- Classes/Application Support/Constants.h
+++ Classes/Application Support/Constants.h
@@ -31,31 +31,7 @@ extern NSString * const kSavedTabOrderKey;
 //#define m_iTunesURL @"http://itunes.com/us/app/TexLege"
 #define m_iTunesURL @"http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=326478866&mt=8"
 
-
-// Legislative Chamber / Legislator Type
-enum kChambers {
-    BOTH_CHAMBERS = 0,
-    HOUSE,
-    SENATE,
-	JOINT	
-};
-
-// Political Party
-enum kParties {
-    kUnknownParty = 0,
-    DEMOCRAT,
-    REPUBLICAN
-};
-
-// Committe Position Roles
-enum kPositions {
-    POS_MEMBER = 0,
-    POS_VICE,
-    POS_CHAIR
-};
-
 enum kURLActionDestinations {
     URLAction_internalBrowser = 0,
     URLAction_externalBrowser,
 };
-
