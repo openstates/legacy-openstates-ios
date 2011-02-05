ed094c653a5ef32e16084635094294772b330cfd
Greg Combs <gcombs@gmail.com>
2011-02-05 08:57:37 +0000

    Overridden classes to fix Apple bugs

---
+++ Classes/Utility Classes/TexLegeNavBar.h
@@ -0,0 +1,17 @@
+//
+//  TexLegeNavBar.h
+//  TexLege
+//
+//  Created by Gregory Combs on 2/5/11.
+//  Copyright 2011 Gregory S. Combs. All rights reserved.
+//
+//	Fixes a retarded bug in iOS 4.2.x that incorrectly deletes and ignores tintColor after opening in a popover/splitview
+
+#import <Foundation/Foundation.h>
+
+
+@interface TexLegeNavBar : UINavigationBar {
+
+}
+
+@end
--- Classes/Utility Classes/UISearchDisplayController+NoHideNav.m
+++ Classes/Utility Classes/UISearchDisplayController+NoHideNav.m
@@ -5,6 +5,10 @@
 //  Created by Gregory Combs on 8/3/10.
 //  Copyright 2010 Gregory S. Combs. All rights reserved.
 //
+//	Description: Whenever you activate a searchDisplayController searchBar located within a pushed navigationcontroller title bar, 
+//		the default iOS behavior (bad) is to hide the nav title bar, including your searchBar, which precludes searching.
+//		This idiotic class serves no purpose but to stop that nonsense. It won't hide the nav title bar when searching.  Retarded, yes?
+//
 
 #import "UISearchDisplayController+NoHideNav.h"
 
@@ -22,6 +26,7 @@
     } else {
         [self.searchBar resignFirstResponder];
     }   
+ 
 }
 
 
