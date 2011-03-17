a2246b93e05d9006433e1d084e75fefb9ea1f6ac
Greg Combs <gcombs@gmail.com>
2011-03-17 19:00:13 +0000

    Added keys and description for metadata contents

--- Classes/Core Data Classes/BillMetadataLoader.h
+++ Classes/Core Data Classes/BillMetadataLoader.h
@@ -26,5 +26,31 @@
 @property (nonatomic,readonly) NSDictionary *metadata;
 @property (nonatomic) BOOL isFresh;
 
+#define kBillMetadataUpdatedKey @"updated"
+#define kBillMetadataContentsKey @"contents"
+#define kBillMetadataTypesKey @"types"
+#define kBillMetadataSubjectsKey @"subjects"
+#define kBillMetadataIDKey @"id"
+#define kBillMetadataTitleKey @"title"
+#define kBillMetadataSubjectsCountKey @"count"
+
+/* metdata contains the following:*
+{
+	"updated":"2011-03-15 15:31:15",
+	"contents":"Texas bill subjects, types, other metadata",
+	"types": [
+		 {"id":1,"title":"HB"},
+		 {"id":2,"title":"HCR"},
+		 { "id":3,"title":"HJR"},
+		 ...
+		 ],
+	"subjects": [
+		 {"id":1,"title":"Agriculture and Food","count":155},
+		 {"id":2,"title":"Animal Rights and Wildlife Issues","count":101},
+		 ...
+		]
+}
+*/
+
 @end
 
