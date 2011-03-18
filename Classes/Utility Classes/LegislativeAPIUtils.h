082d7085fdac3452ebf1d06e89e21ab35453a5fc
Greg Combs <gcombs@gmail.com>
2011-03-18 15:22:46 +0000

    Refactored static urls to common header (openstates, transparencydata, votesmart)

---
+++ Classes/Utility Classes/LegislativeAPIUtils.h
@@ -0,0 +1,16 @@
+/*
+ *  LegislativeAPIUtils.h
+ *  TexLege
+ *
+ *  Created by Gregory Combs on 3/18/11.
+ *  Copyright 2011 Gregory S. Combs. All rights reserved.
+ *
+ */
+
+static NSString *osApiBaseURL =		@"http://openstates.sunlightlabs.com/api/v1";
+static NSString *osApiKey =			@"apikey=350284d0c6af453b9b56f6c1c7fea1f9";
+
+static NSString *transApiBaseURL =	@"http://transparencydata.com/api/1.0";
+
+static NSString *vsApiBaseURL =		@"http://api.votesmart.org";
+static NSString *vsApiKey =			@"key=5fb3b476c47fcb8a21dc2ec22ca92cbb&stateId=TX&o=JSON";
