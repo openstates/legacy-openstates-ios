9c1a314d3ecc4686f9f7da9032b255962370f7da
Greg Combs <gcombs@gmail.com>
2011-03-24 06:56:14 +0000

    error checking

--- Classes/Detail View Controllers/TableCellDataObject.m
+++ Classes/Detail View Controllers/TableCellDataObject.m
@@ -15,14 +15,19 @@
 - (id)initWithDictionary:(NSDictionary *)aDictionary {
 	if ([self init]) {
 		
-		self.entryValue = [aDictionary valueForKey:@"entryValue"];
-		self.entryType = [[aDictionary valueForKey:@"entryType"] integerValue];		
-		self.isClickable = [[aDictionary valueForKey:@"isClickable"] boolValue];
-		self.title = [aDictionary valueForKey:@"title"];
-		self.subtitle = [aDictionary valueForKey:@"subtitle"];
-		if ([aDictionary valueForKey:@"action"])
+		if (!IsEmpty([aDictionary valueForKey:@"entryValue"]))
+			self.entryValue = [aDictionary valueForKey:@"entryValue"];
+		if (!IsEmpty([aDictionary valueForKey:@"entryType"]))
+			self.entryType = [[aDictionary valueForKey:@"entryType"] integerValue];		
+		if (!IsEmpty([aDictionary valueForKey:@"isClickable"]))
+			self.isClickable = [[aDictionary valueForKey:@"isClickable"] boolValue];
+		if (!IsEmpty([aDictionary valueForKey:@"title"]))
+			self.title = [aDictionary valueForKey:@"title"];
+		if (!IsEmpty([aDictionary valueForKey:@"subtitle"]))
+			self.subtitle = [aDictionary valueForKey:@"subtitle"];
+		if (!IsEmpty([aDictionary valueForKey:@"action"]))
 			self.action = [aDictionary valueForKey:@"action"];
-		if ([aDictionary valueForKey:@"parameter"])
+		if (!IsEmpty([aDictionary valueForKey:@"parameter"]))
 			self.parameter = [aDictionary valueForKey:@"parameter"];
 	}
 	return self;
