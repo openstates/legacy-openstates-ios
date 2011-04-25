27583142d54298643986bc9b3660692409382b37
Greg Combs <gcombs@gmail.com>
2011-04-25 16:22:13 +0000

    Explanatory text

--- Classes/Utility Classes/UISearchDisplayController+NoHideNav.m
+++ Classes/Utility Classes/UISearchDisplayController+NoHideNav.m
@@ -5,9 +5,13 @@
 //  Created by Gregory Combs on 8/3/10.
 //  Copyright 2010 Gregory S. Combs. All rights reserved.
 //
-//	Description: Whenever you activate a searchDisplayController searchBar located within a pushed navigationcontroller title bar, 
-//		the default iOS behavior (bad) is to hide the nav title bar, including your searchBar, which precludes searching.
-//		This idiotic class serves no purpose but to stop that nonsense. It won't hide the nav title bar when searching.  Retarded, yes?
+//	Description: Whenever you activate a searchDisplayController 
+//		searchBar located within a pushed navigationcontroller 
+//		title bar, the default iOS behavior (bad) is to hide the 
+//		nav title bar, including your searchBar, which precludes 
+//		searching. This idiotic class serves no purpose but to 
+//		stop that nonsense. It won't hide the nav title bar when 
+//		searching.  Retarded? Yes.  Functional? Yes.
 //
 
 #import "UISearchDisplayController+NoHideNav.h"
