913126281ff600200823fdec0fe76d4dcfacf2f2
Greg Combs <gcombs@gmail.com>
2011-05-02 17:36:49 +0000

    new logging utilities and added future options for orientation checking (using tabbarcontroller)

--- Classes/Utility Classes/UtilityMethods.h
+++ Classes/Utility Classes/UtilityMethods.h
@@ -12,6 +12,28 @@
 
 BOOL IsEmpty(id thing);
 
+NSString * VTPG_DDToStringFromTypeAndValue(const char * typeCode, void * value);
+
+// WARNING: if NO_LOG_MACROS is #defined, then THE ARGUMENT WILL NOT BE EVALUATED
+#ifndef NO_LOG_MACROS
+	#define LOG_EXPR(_X_) do{\
+		__typeof__(_X_) _Y_ = (_X_);\
+		const char * _TYPE_CODE_ = @encode(__typeof__(_X_));\
+		NSString *_STR_ = VTPG_DDToStringFromTypeAndValue(_TYPE_CODE_, &_Y_);\
+		if(_STR_)\
+			NSLog(@"%s = %@", #_X_, _STR_);\
+		else\
+			NSLog(@"Unknown _TYPE_CODE_: %s for expression %s in function %s, file %s, line %d", _TYPE_CODE_, #_X_, __func__, __FILE__, __LINE__);\
+		}while(0)
+
+	#define LOG_NS(...) NSLog(__VA_ARGS__)
+	#define LOG_FUNCTION()	NSLog(@"%s", __func__)
+#else /* NO_LOG_MACROS */
+	#define LOG_EXPR(_X_)
+	#define LOG_NS(...)
+	#define LOG_FUNCTION()
+#endif /* NO_LOG_MACROS */
+
 @class CapitolMap;
 @interface UtilityMethods : NSObject {
 }
@@ -23,7 +45,6 @@ BOOL IsEmpty(id thing);
 	
 + (BOOL) supportsEventKit;
 
-+ (BOOL) supportsMKPolyline;
 + (BOOL) locationServicesEnabled;
 
 + (BOOL) isLandscapeOrientation;
