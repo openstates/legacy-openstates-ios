6697f6a52d27a2df7f0398db91fd0e0a7b109043
Greg Combs <gcombs@gmail.com>
2011-06-29 07:00:02 +0000

    naughty words

--- Classes/Utility Classes/TexLegeNavBar.m
+++ Classes/Utility Classes/TexLegeNavBar.m
@@ -5,7 +5,7 @@
 //  Created by Gregory Combs on 2/5/11.
 //  Copyright 2011 Gregory S. Combs. All rights reserved.
 //
-//	Fixes a retarded bug in iOS 4.2.x that incorrectly deletes and ignores tintColor after opening in a popover/splitview
+//	Fixes an annoying bug in iOS 4.2.x that incorrectly deletes and ignores tintColor after opening in a popover/splitview
 
 #import "TexLegeNavBar.h"
 #import "TexLegeTheme.h"
