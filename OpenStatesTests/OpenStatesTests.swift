5ec053b4b77a966415c2e36f8c47739188c0372f
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-10-01 18:12:22 -0400

    Set up new Swift app.

---
+++ OpenStatesTests/OpenStatesTests.swift
@@ -0,0 +1,36 @@
+//
+//  OpenStatesTests.swift
+//  OpenStatesTests
+//
+//  Created by Daniel Cloud on 10/1/14.
+//  Copyright (c) 2014 Sunlight Foundation. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class OpenStatesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
