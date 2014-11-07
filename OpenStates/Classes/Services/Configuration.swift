02cd03d7c802a20c6ced26e616b3c7aac3bf8ab6
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-11-07 18:39:43 -0500

    Add updated OCDKit version, add Configuration.swift file.

--- OpenStates/Classes/Services/Configuration.swift
+++ OpenStates/Classes/Services/Configuration.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class AppConfiguration {
+    let propertyList:NSDictionary?
+
+    var ocdApiKey:String? {
+        if let key = self.propertyList?.valueForKey("kOCDApiKey") as? String {
+            return key
+        } else {
+            return nil
+        }
+    }
+
+    init() {
+        let bundle = NSBundle.mainBundle()
+        var resourceURL:NSURL? = bundle.URLForResource("ApiKeys", withExtension: "plist")
+
+        if let resourceURL = resourceURL {
+            var data:NSData = NSData(contentsOfURL: resourceURL)!
+            self.propertyList = NSPropertyListSerialization.propertyListWithData(data, options: 0, format: nil, error: nil) as? NSDictionary
+        }
+    }
+
+}
