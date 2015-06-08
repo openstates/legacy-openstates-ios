e5f50e8c80816a7990711833684b32725f3eb93f
Paul Tagliamonte <tag@pault.ag>
2015-06-08 11:50:43 -0400

    Relicense the code from CC-BY-NC to BSD-3
     CC-BY-NC is a nutso choice for code, and it's not an Open Source
     license.
    
     Since the code appears to be copyright assigned to Sunlight, I'm going
     to preform this relicense on behalf of the copyright holder, Sunlight
     Foundation.

--- Classes/Controllers/SLFImprovedRKTableController.m
+++ Classes/Controllers/SLFImprovedRKTableController.m
@@ -6,6 +6,9 @@
 //  Created by Daniel Cloud on 5/29/14.
 //
 //
+// This work is licensed under the BSD-3 License included with this source
+// distribution.
+
 
 #import "SLFImprovedRKTableController.h"
 
--- Classes/Views/BillActionTableViewCell.m
+++ Classes/Views/BillActionTableViewCell.m
@@ -5,6 +5,9 @@
 //  Created by Daniel Cloud on 6/3/14.
 //
 //
+// This work is licensed under the BSD-3 License included with this source
+// distribution.
+
 
 #import "BillActionTableViewCell.h"
 
